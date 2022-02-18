964a8929a83b89356879be2547d5397a7e131b47
Célio <celiofagundes30@gmail.com>
2022-02-18 18:48:41 -0300

    Layout + components

--- panel/pages/index.js
+++ panel/pages/index.js
@@ -1,5 +1,114 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import Title from '../components/Title'
+import Card from '../components/Card'
+import { MdHomeFilled } from 'react-icons/md'
+import Table from '../components/Table'
+const Index = () => {
+  return (
+    <Layout>
+      <Title>Home</Title>
+      <div className='mt-4'>
+        <div className='flex flex-wrap -mx-6'>
+          <Card>
+            <Card.Icon>
+              <MdHomeFilled className='h-8 w-8 text-white' />
+            </Card.Icon>
+            <Card.Data>
+              <Card.Title>tituloe</Card.Title>
+              <Card.Description>descricao</Card.Description>
+            </Card.Data>
+          </Card>
+          <Card>
+            <Card.Icon>
+              <MdHomeFilled className='h-8 w-8 text-white' />
+            </Card.Icon>
+            <Card.Data>
+              <Card.Title>tituloe</Card.Title>
+              <Card.Description>descricao</Card.Description>
+            </Card.Data>
+          </Card>
+          <Card>
+            <Card.Icon>
+              <MdHomeFilled className='h-8 w-8 text-white' />
+            </Card.Icon>
+            <Card.Data>
+              <Card.Title>tituloe</Card.Title>
+              <Card.Description>descricao</Card.Description>
+            </Card.Data>
+          </Card>
+        </div>
+      </div>
 
-export const Index = () => {
-  return <div className='test'>Index</div>
+      <div className='mt-8'></div>
+
+      <div className='flex flex-col mt-8'>
+        <div className='-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
+          <div className='align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200'>
+            <Table>
+              <Table.Head>
+                <Table.Th>Name</Table.Th>
+                <Table.Th>Title</Table.Th>
+                <Table.Th>Status</Table.Th>
+                <Table.Th>Role</Table.Th>
+                <Table.Th></Table.Th>
+              </Table.Head>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Td>
+                    <div className='flex items-center'>
+                      <div className='flex-shrink-0 h-10 w-10'>
+                        <img
+                          className='h-10 w-10 rounded-full'
+                          src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80'
+                          alt=''
+                        />
+                      </div>
+
+                      <div className='ml-4'>
+                        <div className='text-sm leading-5 font-medium text-gray-900'>
+                          John Doe
+                        </div>
+                        <div className='text-sm leading-5 text-gray-500'>
+                          john@example.com
+                        </div>
+                      </div>
+                    </div>
+                  </Table.Td>
+                  <Table.Td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                    <div className='text-sm leading-5 text-gray-900'>
+                      Software Engineer
+                    </div>
+                    <div className='text-sm leading-5 text-gray-500'>
+                      Web dev
+                    </div>
+                  </Table.Td>
+
+                  <Table.Td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                    <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
+                      Active
+                    </span>
+                  </Table.Td>
+
+                  <Table.Td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500'>
+                    Owner
+                  </Table.Td>
+
+                  <Table.Td className='px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium'>
+                    <a
+                      href='#'
+                      className='text-indigo-600 hover:text-indigo-900'
+                    >
+                      Edit
+                    </a>
+                  </Table.Td>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
+export default Index
